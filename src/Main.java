6c52fd856226229c84e611e10d48ef95c43cce39
Nagaraj1007 <nagaraj.20it@sonatech.ac.in>
2023-07-18 17:14:19 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 
 public class Main {
-
+// Fifth bit only differs in 8 bits of the binary numbers of ascii numbers...
+// So we are checking
     public static String toggleResult(String word){
         char charArrayOfString[] = word.toCharArray();
         for (int iterator = 0 ; iterator < charArrayOfString.length ; iterator++){
-            charArrayOfString[iterator] = (charArrayOfString[iterator] >= 65 && charArrayOfString[iterator] <= 90) ? (char)( charArrayOfString[iterator] + 32 ) : (char) ( charArrayOfString[iterator] - 32 );
+            charArrayOfString[iterator] ^= 32 ;
         }
         return new String(charArrayOfString);
     }
